58e2dc54e1db32e053d41917a1ee06dae728ea3c
iphilka <i376765616@yandex.ru>
2024-05-05 19:49:22 +0500

    Правка под ES6, добавление общего контекста выполнения БД

---
+++ config/config.js
@@ -0,0 +1,23 @@
+export default {
+  development: {
+    username: 'admin',
+    password: 'password',
+    database: 'master-bards-db',
+    host: 'db',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'admin',
+    password: 'password',
+    database: 'master-bards-db',
+    host: 'db',
+    dialect: 'postgres',
+  },
+  production: {
+    username: 'admin',
+    password: 'password',
+    database: 'master-bards-db',
+    host: 'db',
+    dialect: 'postgres',
+  },
+};
--- routes/user.js
+++ routes/user.js
@@ -1,14 +1,42 @@
 import express from 'express';
-
+import User from '../models/user.js';
 
 const userRouter = express.Router();
 
-userRouter.get('/:id', (req, res) => {
-    res.send('Welcome to user root!');
+userRouter.get('/', async (req, res) => {
+  const users = await User.findAll({
+    attributes: [
+      'id',
+      'firstName',
+      'secondName',
+      'middleName',
+      'birthday',
+      'artistName',
+      'email',
+      'phone',
+      'hashPassword',
+      'vkLink',
+    ],
+  });
+  res.send(`Метод полученимя всех пользователей не реализован! test: ${users}`);
 });
 
-userRouter.get('/qr', (req, res) => {
-    res.send('QR!');
+userRouter.post('/', async (req, res) => {
+  res.send('Метод создания нового пользователя не реализован!');
+});
+
+userRouter.get('/:id', async (req, res) => {
+  res.send('Метод получения информации о конкретном пользователе не реализован!');
+});
+
+userRouter.put('/:id', async (req, res) => {
+  res.send('Метод редактирования конкретного пользователя не реализован!');
+});
+
+userRouter.delete('/:id', async (req, res) => {
+  // по факту на данный момент мы не будем удалять пользователя,
+  // нужно в базе данных ставить ему флаг isDeleted : true
+  res.send('Метод удаоения пользователя не реализован!');
 });
 
 export default userRouter;
